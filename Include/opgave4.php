db7cc773feb67f3c00fb450f08b3967857891a7c
Mathias Ry <math274k@edu.campusvejle.dk>
2019-08-15 14:54:00 +0200

    not important

--- Include/opgave4.php
+++ Include/opgave4.php
@@ -11,20 +11,19 @@ function findBoyoInArray() {
 		"bobwix",
 		"tobwix",
 		"jacwix" );
-	$tempImplode = implode($delimiter, $elever);
+	$tempImplode = implode( $delimiter, $elever );
 
-//	echo $tempImplode;
-  	ucwords($tempImplode, $delimiter);
+	//	echo $tempImplode;
+	ucwords( $tempImplode, $delimiter );
 	echo $tempImplode;
-	$newStudentArray = explode($delimiter, $tempImplode);
-		print_r($newStudentArray);
+	$newStudentArray = explode( $delimiter, $tempImplode );
+	print_r( $newStudentArray );
 
-	/*if ( in_array( $find, $newStudentArray ) ) {
+	if ( in_array( $find, $newStudentArray ) ) {
 
 		echo " \n Personen er fundet og er nr: " . array_search( $find, $newStudentArray ) . " i array'et";
 	} else {
 		echo nl2br( "F i chatten boys" );
-	}*/
-	
+	}
 }
 ?>
